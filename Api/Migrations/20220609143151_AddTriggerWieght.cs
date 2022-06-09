6ce384348ede200342e8012094cda8fc866a14ef
cebilon123 <michalkowal567@gmail.com>
2022-06-09 16:33:13 +0200

    Dodanie procedury zliczania wagi

---
+++ Api/Migrations/20220609143151_AddTriggerWieght.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Migrations
+{
+    public partial class AddTriggerWieght : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"CREATE TRIGGER trgUpdateOrderWeight
+                                    ON dbo.OrderedProducts
+                                    AFTER INSERT, UPDATE
+                                    AS
+                                    BEGIN
+	                                    UPDATE dbo.Orders
+	                                    SET dbo.Orders.Weight = (SELECT SUM(dbo.OrderedProducts.Count * dbo.OrderedProducts.Weight) FROM dbo.OrderedProducts
+							                                    inner join inserted on inserted.OrderId = dbo.OrderedProducts.OrderId
+							                                    WHERE dbo.OrderedProducts.OrderId = inserted.OrderId)
+	                                    FROM inserted
+	                                    WHERE dbo.Orders.id = inserted.OrderId
+                                    END
+                                    ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DROP TRIGGER trgUpdateOrderWeight");
+        }
+    }
+}
